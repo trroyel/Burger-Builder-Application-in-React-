092e744473bd055f1d077e9540298b023b0357bb
trroyel <t.r.royel@gmail.com>
2018-08-10 22:18:52 +0600

    Sharing the validation method

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,22 @@ export const updateObject = (oldObject, updatedObject) => {
         ...updatedObject
     };
 };
+
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+    //This checking is not compolsary if i use validation 
+    //property(deliveryMethod) in thr state
+    if (!rules) {
+        return true;
+    }
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+}
