01fc538785cfdca236120bafaf17c06d7827b4b7
trroyel <t.r.royel@gmail.com>
2018-08-11 13:29:23 +0600

    Test reducer

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -21,4 +21,9 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('Should render three <NavigationItems /> elements if  authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.contains(<NavigationItem link="/logout" > Logout</NavigationItem>)).toEqual(true);
+    });
 });
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import * as burgerBuilderActions from '../../store/actions/index';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 
     // constructor(){
     //     super();
