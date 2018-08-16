f004c95753cf603070ae82edb2e8f2a4a98f6353
trroyel <t.r.royel@gmail.com>
2018-08-16 19:13:02 +0600

    Handling authentication with Saga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -16,5 +16,9 @@ export {
     logout,
     setAuthRedirectPath,
     authCheckState,
-    logoutSucceed
+    logoutSucceed,
+    authStart,
+    authSuccess,
+    authFail,
+    checkAuthTimeout
 } from './auth';
